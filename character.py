b2c8824da690ffd27962eb95835b7ecd834dc570
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2024-09-03 17:59:02 +0000

    fixed mistake

--- character.py
+++ character.py
@@ -1,7 +1,8 @@
 class Character:
-    def __init__(self, name, health, inventory)
+    def __init__(self, name, health, inventory):
         self.name = name
         self.health = health
         self.inventory = inventory
 
-    
+    def take_damage(self, damage):
+        self.health -= damage
