326d4c2d0e65e70861651977c5f98f7d41bf6819
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2024-09-04 18:06:10 +0000

    started writing story

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
+print("You are in an abandoned building.")
+print("You do not remember how you got here.")
+print("There are no signs. ")
+answerA = input("Where will you go now? (forward/back/left/right)")
 
+if answerA == "":
+    print("You start walking forward. Through the trees, you can just barely see the sun start to set.")
+
+elif answerA == "back":
+    answerb = input("Are you sure?")
+    if answerb == "yes":
+        print("I wouldn't recommend it, but alright.")
+        print("")
+    elif answerb == "no":
+        print("Where will you go now? (forward/back/left/right)")
+
+elif answerA == "left":
+    print("")
+
+elif answerA == "right":
+    print("")
