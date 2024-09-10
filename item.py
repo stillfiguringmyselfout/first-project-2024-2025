5f131c1f254c19c8724331c521de8ef2abd322eb
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2024-09-10 17:59:49 +0000

    added flashlight functionality

--- item.py
+++ item.py
@@ -1,7 +1,15 @@
-class Item:
+class flashlight:
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.on = False
+        self.broken = False
 
-    
+    def turn_on(self):
+        if self.on == False and self.broken == False:
+            self.on = True
+        elif self.broken == True:
+            print("The flashlight will not turn on.")
+        elif self.on == True:
+            print("The flashlight is already on.")
